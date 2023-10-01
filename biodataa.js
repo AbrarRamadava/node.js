4a24a989b96de17ba45ccb3440c0dd1782525cdd
unknown <abrarramadava26@gmail.com>
2023-10-01 23:15:20 +0700

    first commit

---
+++ biodataa.js
@@ -0,0 +1,9 @@
+var firstName = "Abrar Ramadava"; 
+var lastName = "Algadri Suriawan"; 
+var age = 21; 
+var isMarried = false; 
+
+console.log("Nama Depan: " + firstName);
+console.log("Nama Belakang: " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + isMarried);
